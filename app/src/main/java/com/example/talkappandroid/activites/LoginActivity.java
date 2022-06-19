15c75f99a506c6a47c99190dbfe50a0dc2862d8e
Amit Paz <amitpaz97@gmail.com>
2022-06-19 12:07:53 +0300

    set server

--- app/src/main/java/com/example/talkappandroid/activites/LoginActivity.java
+++ app/src/main/java/com/example/talkappandroid/activites/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         bindViews();
-        setServer();
+        //setServer();
         setListeners();
 
     }
